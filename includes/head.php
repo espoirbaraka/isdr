2d5e4cd0dd86d0b99888184dad5f5e0ff24ace07
Baraka Bigega Espoir <49196039+espoirbaraka@users.noreply.github.com>
2022-03-05 12:09:38 +0200

    Best login

---
+++ includes/head.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>ISDR-GL</title>
+  <link rel="shortcut icon" type="image/x-icon" href="assets/images/logo.png">
+  <link href="https://fonts.googleapis.com/css?family=Karla:400,700&amp;display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="cdn.jsdelivr.net/npm/%40mdi/font%404.8.95/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/login.css">
+</head>
---
+++ ui/ui_login.php
@@ -0,0 +1,30 @@
+<body>
+  <main class="d-flex align-items-center min-vh-100 py-3 py-md-0">
+    <div class="container">
+      <div class="card login-card">
+        <div class="row no-gutters">
+          <div class="col-md-5">
+            <img src="assets/images/etudiant.jpg" alt="login" class="login-card-img">
+            
+          </div>
+          <div class="col-md-7">
+            <div class="card-body">
+              <div class="brand-wrapper">
+                <img src="assets/images/logo.png" alt="logo" class="logo">
+              </div>
+              <p class="login-card-description">Connectez-vous</p>
+              <form action="#!">
+                  <div class="form-group">
+                    <label for="matricule" class="sr-only">Marticule</label>
+                    <input type="text" name="matricule" id="matricule" class="form-control" placeholder="Matricule">
+                  </div>
+                  <input name="login" id="login" class="btn btn-block login-btn mb-4" style="background-color: #30afba;" type="button" value="Se connecter">
+                </form>
+                
+            </div>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  </main>
